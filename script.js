da9d06af053cf8fc6e93ef44e0d42c49b8be0b64
MAHDIA001 <mahdianaiebi@gmail.com>
2021-08-02 10:20:47 -0700

    some more styles and a little js code

---
+++ script.js
@@ -0,0 +1,4 @@
+let btn = document.getElementById("btn");
+let color = document.getElementById("colors");
+let array = ['rgb(10,200,70)','#facfa2','#a8a8a8a8','#333','rgb(100,150,200)']
+btn.addEventListener('click'()=>{})
