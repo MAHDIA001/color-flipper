7faf737899e3c7da005b1a6d6bde90573998202d
MAHDIA001 <mahdianaiebi@gmail.com>
2021-08-02 10:32:29 -0700

    static color generator!

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let btn = document.getElementById("btn");
 let color = document.getElementById("colors");
 let array = ['rgb(10,200,70)','#facfa2','#a8a8a8a8','#333','rgb(100,150,200)']
-btn.addEventListener('click'()=>{})
+let event = btn.addEventListener('click',()=>{
+   
+         const rand =Math.floor(Math.random()*array.length);
+         document.body.style.backgroundColor = array[rand]
+
+    color.textContent = array[rand]
+})
